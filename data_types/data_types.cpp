366f08256d7d5039b48c20f7a78f4e36bcf1cde2
sudo-x86x64 <ivanovpavel.ips@gmail.com>
2020-03-29 22:50:56 -0700

    First try of vector type

---
+++ data_types/data_types.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int     x = -1;
+    double  pi = 3.14;
+    bool    logical_value = true;
+    char    symbol = 'Z';
+    string  helloWorld = "Hello, World\n";
+    vector<int> nums = {1, 3, 5 , 7};
+
+    cout << nums.size() << endl;
+    
+    return 0;
+}
