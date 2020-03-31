efbe8d14e97c202424c902d5f1c3934630f1d1fd
sudo-x86x64 <ivanovpavel.ips@gmail.com>
2020-03-30 21:09:02 -0700

    C++17 string comparsion exersice

---
+++ data_types/string_type/string.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main () {
+    string a = "This is string a!";
+    string b = "This is string b!";
+
+    if (a == b) cout << "Strings are equal" << endl;
+    else        cout << "Strings are not equal" << endl;
+
+    // Strings in C++ are compared lexicographically
+    if (a > b)  cout << "String a has higher value" << endl;
+    else        cout << "String a has lower value" << endl;
+
+    return 0;
+}
