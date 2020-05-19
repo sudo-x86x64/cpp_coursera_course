b4d9792d8b2e4e302b86481a06d7968a719f74e2
sudo-x86x64 <ivanovpavel.ips@gmail.com>
2020-05-19 01:13:38 -0700

    Solution for "struct lectureTitle" assignment

---
+++ assignments/week4/sturct_lecturetitle/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+struct Specialization {
+    string specialization;
+    explicit Specialization(string specializationName) {specialization = specializationName;}
+};
+struct Course {
+    string course;
+    explicit Course(string courseName) {course = courseName;}
+};
+struct Week {
+    string week;
+    explicit Week(string weekName) {week = weekName;}
+};
+
+struct LectureTitle {
+    string specialization;
+    string course;
+    string week;
+
+    LectureTitle (Specialization inputSpecialization, Course inputCourse, Week inputWeek) {
+        specialization = inputSpecialization.specialization;
+        course = inputCourse.course;
+        week = inputWeek.week;
+    }
+};
